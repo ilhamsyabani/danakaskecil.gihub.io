d03a6e7dddfb9c27210ff85f984bff4b4715a30e
llham sy <ilham.syabani@student.uny.ac.id>
2023-09-13 19:12:48 +0700

    add button function

--- app.js
+++ app.js
@@ -7,119 +7,177 @@ document.addEventListener("DOMContentLoaded", function () {
     tambahLaporanButton.addEventListener('click', () => {
         // Menampilkan formulir dan tabel jenis anggaran
         formLaporan.style.display = 'block';
-    });
+    })
+});
 
-    // Mengecek apakah ada data sebelumnya di session storage
-    const storedData = JSON.parse(sessionStorage.getItem("storedData")) || [];
+function validateForm() {
+    var tanggal = document.getElementById("tanggal").value;
+    var bukti = document.getElementById("bukti").value;
+    var keterangan = document.getElementById("keterangan").value;
+    var penerimaan = document.getElementById("penerimaan").value;
+    var pengeluaran = document.getElementById("pengeluaran").value;
+    var akun = document.getElementById("akun").value;
+    var nilai = document.getElementById("nilai").value;
+    if (tanggal == "" && bukti == "") {
+        alert("tolong isi tanggal dan no bukti");
+        return false;
+    }
+    return true;
+}
 
-    // Menampilkan data yang sudah ada di session storage
-    for (const data of storedData) {
-        addDataToTable(data);
+function showData() {
+    var reportList;
+    if (localStorage.getItem("reportList") == null) {
+        reportList = [];
+    } else {
+        reportList = JSON.parse(localStorage.getItem("reportList"));
     }
 
-    dataForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+    var html = "";
 
-        const tanggalInput = document.getElementById("tanggal");
-        const buktiInput = document.getElementById("bukti");
-        const keteranganInput = document.getElementById("keterangan");
-        const penerimaanInput = document.getElementById("penerimaan");
-        const pengeluaranInput = document.getElementById("pengeluaran");
-        const akunInput = document.getElementById("akun");
-        const nilaiInput = document.getElementById("nilai");
-
-        const tanggal = tanggalInput.value.trim();
-        const bukti = buktiInput.value.trim();
-        const keterangan = keteranganInput.value.trim();
-        const penerimaan = penerimaanInput.value.trim();
-        const pengeluaran = pengeluaranInput.value.trim();
-        const akun = akunInput.value.trim();
-        const nilai = nilaiInput.value.trim();
-
-        if (tanggal !== "" && bukti !== "") {
-            const newData = {
-                tanggal: tanggal,
-                bukti: bukti,
-                keterangan: keterangan,
-                penerimaan: penerimaan,
-                pengeluaran: pengeluaran,
-                akun: akun,
-                nilai: nilai,
-            };
-
-            // Menambahkan data ke tabel
-            addDataToTable(newData);
-
-            // Menyimpan data ke session storage
-            storedData.push(newData);
-            sessionStorage.setItem("storedData", JSON.stringify(storedData));
-
-            // Mengosongkan formulir
-            tanggalInput.value = "";
-            buktiInput.value = "";
-            keteranganInput.value = "";
-            penerimaanInput.value = "";
-            pengeluaranInput.value = "";
-            akunInput.value = "";
-            nilaiInput.value = "";
-            formLaporan.style.display = 'none';
+    reportList.forEach(function (element, index) {
+        html += "<tr>";
+        html += "<td>" + element.tanggal + "</td>";
+        html += "<td>" + element.bukti + "</td>";
+        html += "<td>" + element.keterangan + "</td>";
+        html += "<td>" + element.penerimaan + "</td>";
+        html += "<td>" + element.pengeluaran + "</td>";
+        if (element.akun === "1") {
+            html += "<td>" + element.nilai + "</td>";
+        } else {
+            html += "<td>" + " "+ "</td>"
         }
+        if (element.akun === "2") {
+            html += "<td>" + element.nilai + "</td>";
+        } else{
+            html += "<td>" + " "+ "</td>"
+        } 
+        if (element.akun === "3") {
+            html += "<td>" + element.nilai + "</td>";
+        } else {
+            html += "<td>" + " "+ "</td>"
+        }
+        if (element.akun === "4") {
+            html += "<td>" + element.nilai + "</td>";
+        } else {
+            html += "<td>" + " "+ "</td>"
+        }
+        if (element.akun === "5") {
+            html += "<td>" + element.nilai + "</td>";
+        } else {
+            html += "<td>" + " "+ "</td>"
+        }
+        html += '<td><button class="delete-btn" onclick="deleteData(' + index + ')">Hapus</button><button class="edit-btn" onclick="updateData(' + index + ')">Edit</button></td>';
+        html += "</tr>";
     });
 
-    function addDataToTable(data) {
-        const row = dataTable.insertRow(2);
-        const cellTanggal = row.insertCell(0);
-        const cellBukti = row.insertCell(1);
-        const cellKeterangan = row.insertCell(2);
-        const cellPenerimaan = row.insertCell(3);
-        const cellPengeluaran = row.insertCell(4);
+    document.querySelector("#data-table tbody").innerHTML = html;
+}
 
-        // Tambahkan sel-sel untuk setiap jenis akun yang mungkin ada
-        const cellAkun1 = row.insertCell(5);
-        const cellAkun2 = row.insertCell(6);
-        const cellAkun3 = row.insertCell(7);
-        const cellAkun4 = row.insertCell(8);
-        const cellAkun5 = row.insertCell(9);
-        const cellTidakan = row.insertCell(10);
-        const divItem = document.createElement("div");
-        divItem.classList.add("div-item");
-        divItem.innerHTML = `
-            <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
-        `;
+document.onload = showData();
 
-        // Menambahkan event listener untuk tombol hapus
-        const deleteButton = divItem.querySelector(".delete-btn");
-        deleteButton.addEventListener("click", function () {
-            const rowToDelete = this.closest("tr");
+function addData() {
+    if (validateForm() == true) {
+        var tanggal = document.getElementById("tanggal").value;
+        var bukti = document.getElementById("bukti").value;
+        var keterangan = document.getElementById("keterangan").value;
+        var penerimaan = document.getElementById("penerimaan").value;
+        var pengeluaran = document.getElementById("pengeluaran").value;
+        var akun = document.getElementById("akun").value;
+        var nilai = document.getElementById("nilai").value;
 
-            // Menghapus baris dari tabel
-            if (rowToDelete) {
-                rowToDelete.remove();
-            }
+        var reportList;
+        if (localStorage.getItem("reportList") == null) {
+            reportList = [];
+        } else {
+            reportList = JSON.parse(localStorage.getItem("reportList"));
+        }
+
+        reportList.push({
+            tanggal: tanggal,
+            bukti: bukti,
+            keterangan: keterangan,
+            penerimaan: penerimaan,
+            pengeluaran: pengeluaran,
+            akun: akun,
+            nilai: nilai,
         });
 
+        localStorage.setItem("reportList", JSON.stringify(reportList));
 
+        showData();
 
-        cellTanggal.textContent = data.tanggal;
-        cellBukti.textContent = data.bukti;
-        cellKeterangan.textContent = data.keterangan;
-        cellPenerimaan.textContent = data.penerimaan;
-        cellPengeluaran.textContent = data.pengeluaran;
-        cellTidakan.appendChild(divItem);
-
-
-        // Tambahkan nilai ke sel akun yang sesuai
-        if (data.akun = "1") {
-            cellAkun1.textContent = data.nilai;
-        } else if (data.akun = "2") {
-            cellAkun2.textContent = data.nilai;
-        } else if (data.akun = "3") {
-            cellAkun3.textContent = data.nilai;
-        } else if (data.akun = "4") {
-            cellAkun4.textContent = data.nilai;
-        } else if (data.akun = "5") {
-            cellAkun5.textContent = data.nilai;
-        }
-
+        document.getElementById("tanggal").value = "";
+        document.getElementById("bukti").value = "";
+        document.getElementById("keterangan").value = "";
+        document.getElementById("penerimaan").value = "";
+        document.getElementById("pengeluaran").value = "";
+        document.getElementById("akun").value = "";
+        document.getElementById("nilai").value = "";
+        document.getElementById('form-laporan-kas-kecil').style.display = "none";
     }
-});
+}
+
+//Delete data
+function deleteData(index){
+    var reportList;
+    if (localStorage.getItem("reportList") == null) {
+        reportList = [];
+    } else {
+        reportList = JSON.parse(localStorage.getItem("reportList"));
+    }
+
+    reportList.splice(index, 1);
+    localStorage.setItem("reportList", JSON.stringify(reportList));
+    showData();
+}
+
+//Update Data
+function updateData(index) {
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("update").style.display = "block";
+    document.getElementById('form-laporan-kas-kecil').style.display = "block";
+
+    var reportList;
+    if (localStorage.getItem("reportList") == null) {
+        reportList = [];
+    } else {
+        reportList = JSON.parse(localStorage.getItem("reportList"));
+    }
+
+    document.getElementById("tanggal").value = reportList[index].tanggal;
+    document.getElementById("bukti").value = reportList[index].bukti;
+    document.getElementById("keterangan").value = reportList[index].keterangan;
+    document.getElementById("penerimaan").value = reportList[index].penerimaan;
+    document.getElementById("pengeluaran").value = reportList[index].pengeluaran;
+    document.getElementById("akun").value = reportList[index].akun;
+    document.getElementById("nilai").value = reportList[index].nilai;
+
+    document.querySelector("#update").onclick = function () {
+        if (validateForm() == true) {
+            reportList[index].tanggal = document.getElementById("tanggal").value;
+            reportList[index].bukti = document.getElementById("bukti").value;
+            reportList[index].keterangan = document.getElementById("keterangan").value;
+            reportList[index].penerimaan = document.getElementById("penerimaan").value;
+            reportList[index].pengeluaran = document.getElementById("pengeluaran").value;
+            reportList[index].akun = document.getElementById("akun").value;
+            reportList[index].nilai = document.getElementById("nilai").value;
+
+            localStorage.setItem("reportList", JSON.stringify(reportList));
+
+            showData();
+
+            document.getElementById("tanggal").value = "";
+            document.getElementById("bukti").value = "";
+            document.getElementById("keterangan").value = "";
+            document.getElementById("penerimaan").value = "";
+            document.getElementById("pengeluaran").value = "";
+            document.getElementById("akun").value = "";
+            document.getElementById("nilai").value = "";
+
+            document.getElementById("submit").style.display = "block";
+            document.getElementById("update").style.display = "none";
+            document.getElementById('form-laporan-kas-kecil').style.display = "none";
+        }
+    }
+}
